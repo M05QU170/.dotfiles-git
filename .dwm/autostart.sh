7730a88cd1aeddef7a0a6ad82d3ac50cdc2fb703
Tomas <tomasgab@gmail.com>
2020-06-07 17:48:55 +0100

    dwm autostart script

---
+++ .dwm/autostart.sh
@@ -0,0 +1,19 @@
+#! /bin/bash 
+
+fix_xcursor &
+picom -b &
+nm-applet &
+blueman-tray &
+fcitx &
+xfce4-power-manager &
+redshift-gtk &
+variety &
+setxkbmap -layout gb &
+#pamac-tray &
+volumeicon &
+xfsettingsd &
+xfce4-volumed-pulse &
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+gnome-keyring-daemon --start --components=pkcs11 &
+
+dwmblocks &
