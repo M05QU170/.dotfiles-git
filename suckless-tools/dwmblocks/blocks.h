b2b30dab5f523055cfeb144bb3f1e2bcc492d74d
Tomas <tomasgab@gmail.com>
2020-06-12 15:17:46 +0100

    suckless tools

---
+++ suckless-tools/dwmblocks/blocks.h
@@ -0,0 +1,19 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/	 	/*Update Interval*/	/*Update Signal*/
+    //  {" 🐧 ", "dwm-kernel",		    360,		        2}, 
+
+	// {" 🔺 ", "dwm-upt",		        60,		            2}, 
+
+//	{" 📦 ", "dwm-pacupdate",		360,		        9},
+	
+	{"", "dwm-cpu",	        5,		            10},
+//	{" 💻 ", "dwm-memory",	        6,		            1},
+	{"", "dwm-memory",	        5,		            1},
+	//  {" 🔊 ", "dwm-volume",			2,		            10}, 
+
+	{" 🕑 ", "dwm-clock",			60,		            0},
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim = '|';
